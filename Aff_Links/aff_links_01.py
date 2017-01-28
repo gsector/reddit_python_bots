039dd743b5b7e82bbe30c1ecb15f3e1f47a2eef9
Stephen <stephen@gsector.net>
2017-01-28 11:15:20 -0800

    updates

--- Aff_Links/aff_links_01.py
+++ Aff_Links/aff_links_01.py
@@ -1,35 +1,102 @@
 import praw
 import re
 import configparser
+from multiprocessing import Process
 
 
-def main():
+def process_comments():
+	print('Processing Comments...')
+
 	config = configparser.RawConfigParser()
 	config.read('aff_links_01.cfg')
 
-	reddit_user = config.get('USER','reddit_user')
-	reddit_password = config.get('USER','reddit_password')
+	reddit_user = config.get('USER', 'reddit_user')
+	reddit_password = config.get('USER', 'reddit_password')
 
-	app_secret = config.get('APP','app_secret')
-	app_name = config.get('APP','app_name')
-	app_client_id = config.get('APP','app_client_id')
+	app_secret = config.get('APP', 'app_secret')
+	app_name = config.get('APP', 'app_name')
+	app_client_id = config.get('APP', 'app_client_id')
 
 	reddit = praw.Reddit(client_id=app_client_id, client_secret=app_secret,
-	                     password=reddit_password, user_agent=app_name,
-	                     username=reddit_user)
+						password=reddit_password, user_agent=app_name,
+						username=reddit_user)
 
-	subr = config.get('SUBREDDIT','sub')
-
-	link_re = re.compile('[htps:\/w.]*[\S]*amazon[\S]*[a-zA-Z0-9=]')
-	tag_re = re.compile('\?[tT][aA]{0,1}[gG]{0,1}=[\S]*-[0-9][0-9]')
+	subr = config.get('SUBREDDIT', 'sub')
 
 	# Process comments in a specific subreddit
-	print('Running....')
 	for comment in reddit.subreddit(subr).stream.comments():
-		print(comment.body)
-		for link in re.findall(link_re, comment.body):
-			print(link)
+		amazon_links = find_amazon_links(comment.body)
+		if amazon_links:
+			print('Found Amazon link(s) in comment: ' + str(amazon_links))
+			if find_affiliate_tags(amazon_links):
+				print('** Affiliate link found! **')
+				# comment.reply('Warning! An Affliate Link has been Detected in your post.')
+
+
+def process_submissions():
+	print('Processing Submissions...')
+
+	config = configparser.RawConfigParser()
+	config.read('aff_links_01.cfg')
+
+	reddit_user = config.get('USER', 'reddit_user')
+	reddit_password = config.get('USER', 'reddit_password')
+
+	app_secret = config.get('APP', 'app_secret')
+	app_name = config.get('APP', 'app_name')
+	app_client_id = config.get('APP', 'app_client_id')
+
+	reddit = praw.Reddit(client_id=app_client_id, client_secret=app_secret,
+						password=reddit_password, user_agent=app_name,
+						username=reddit_user)
+
+	subr = config.get('SUBREDDIT', 'sub')
+
+	# Process submissions in a specific subreddit
+	for submission in reddit.subreddit(subr).stream.submissions():
+		submission_text = submission.selftext + ' - ' + submission.url
+		amazon_links = find_amazon_links(submission_text)
+		if amazon_links:
+			print('Found Amazon link(s) in submission: ' + str(amazon_links))
+			if find_affiliate_tags(amazon_links):
+				print('** Affiliate link found! **')
+				# submission.reply('Warning! An Affliate Link has been Detected in your post.')
+
+
+
+def find_affiliate_tags(link_list):
+	'''
+			(string) -> boolean
+
+			Returns True when an affiliate link is found 
+			within one of the links in the list of links.
+			Else, returns False.
+	'''
+
+	tag_re = re.compile('\?[tT][aA]{0,1}[gG]{0,1}=[\S]*-[0-9][0-9]')
+
+	for link in link_list:
+		if re.findall(tag_re, link):
+			return True
+	return False
+
+
+def find_amazon_links(blob):
+	'''
+			(string) -> List
+
+			Returns a list of Amazon.com links within a string of text.
+	'''
+
+	link_re = re.compile('amazon\.[\S]*[a-zA-Z0-9=]')
+	link_list = list()
+
+	for link in re.findall(link_re, blob):
+		link_list.append(link)
+
+	return link_list
+
 
 if __name__ == '__main__':
-	main()
-	print('Done running')
+	Process(target=process_comments).start()
+	Process(target=process_submissions).start()
--- Amazon_Smile/amazon_smile_01.py
+++ Amazon_Smile/amazon_smile_01.py
@@ -6,12 +6,12 @@ import configparser
 config = configparser.RawConfigParser()
 config.read('amazon_smile_01.cfg')
 
-reddit_user = config.get('USER','reddit_user')
-reddit_password = config.get('USER','reddit_password')
+reddit_user = config.get('USER', 'reddit_user')
+reddit_password = config.get('USER', 'reddit_password')
 
-app_secret = config.get('APP','app_secret')
-app_name = config.get('APP','app_name')
-app_client_id = config.get('APP','app_client_id')
+app_secret = config.get('APP', 'app_secret')
+app_name = config.get('APP', 'app_name')
+app_client_id = config.get('APP', 'app_client_id')
 
 reddit = praw.Reddit(client_id=app_client_id, client_secret=app_secret,
                      password=reddit_password, user_agent=app_name,
@@ -19,80 +19,82 @@ reddit = praw.Reddit(client_id=app_client_id, client_secret=app_secret,
 
 p = re.compile('([htps:\/w.]+amazon.com\/.*)')
 
-def last_char_test(c):
-	'''
-		char -> boolean
 
-		Returns true when the last character in a string
-		is valid for a url last character
-	'''
-	return c.isalpha() or c.isdigit() or c in '?='
+def last_char_test(c):
+    '''
+            char -> boolean
+
+            Returns true when the last character in a string
+            is valid for a url last character
+    '''
+    return c.isalpha() or c.isdigit() or c in '?='
+
 
 def extract_url(raw_url):
-	'''
-		string -> string
+    '''
+            string -> string
 
-		Takes a string, extracts the first part before a space if there is one,
-		and then removes last characters until it hits a valid url character.
-	'''
+            Takes a string, extracts the first part before a space if there is one,
+            and then removes last characters until it hits a valid url character.
+    '''
 
-	try:
-		all_pieces = raw_url.split(' ')
-		only_url = all_pieces[0]
-	except:
-		only_url = raw_url
+    try:
+        all_pieces = raw_url.split(' ')
+        only_url = all_pieces[0]
+    except:
+        only_url = raw_url
 
-	while last_char_test(only_url[-1]) is False:
-		try:
-			only_url = only_url[:-1]
-		except:
-			return None
+    while last_char_test(only_url[-1]) is False:
+        try:
+            only_url = only_url[:-1]
+        except:
+            return None
 
-	return only_url
+    return only_url
 
 
 def create_reply(url):
-	text = 'AmazonSmile Link'
-	reftag = '/?tag=smile.reddit-20'
-	tagline = '\n\nUse this AmazonSmile link to donate a part of your purchase to charity.'
+    text = 'AmazonSmile Link'
+    reftag = '/?tag=smile.reddit-20'
+    tagline = '\n\nUse this AmazonSmile link to donate a part of your purchase to charity.'
 
-	# reply_text = '[AmazonSmile Link]({}{})^^[no-ref]({})'.format(url,reftag,url)
-	# reply_text = '[AmazonSmile Link]({}{})'.format(url,reftag)
-	reply_text = '[AmazonSmile Link]({})'.format(url)
-	reply_text = reply_text + tagline
+    # reply_text = '[AmazonSmile Link]({}{})^^[no-ref]({})'.format(url,reftag,url)
+    # reply_text = '[AmazonSmile Link]({}{})'.format(url,reftag)
+    reply_text = '[AmazonSmile Link]({})'.format(url)
+    reply_text = reply_text + tagline
 
-	return reply_text
+    return reply_text
 
 
 def process_comment(comment):
-	amazon_matches = p.findall(comment)
-	
-	# If 1 amazon link is found...
-	if len(amazon_matches) == 1:
-		# Clean up the url....
-		raw_url = amazon_matches[0]
-		url = extract_url(raw_url)
-		if 'smile.amazon' in url:
-			return None
-		# Add smile link stuff
-		url = url.replace('amazon.', 'smile.amazon.')
-		if 'www.smile' in url:
-			url = url.replace('www.smile.', 'smile.')
+    amazon_matches = p.findall(comment)
 
-		return create_reply(url)
-	else:
-		return None
+    # If 1 amazon link is found...
+    if len(amazon_matches) == 1:
+        # Clean up the url....
+        raw_url = amazon_matches[0]
+        url = extract_url(raw_url)
+        if 'smile.amazon' in url:
+            return None
+        # Add smile link stuff
+        url = url.replace('amazon.', 'smile.amazon.')
+        if 'www.smile' in url:
+            url = url.replace('www.smile.', 'smile.')
+
+        return create_reply(url)
+    else:
+        return None
 
 
-def post_comment(comment,reply_text):
-	try:
-		comment.reply(reply_text)
-		print('Comment Posted')
-	except:
-		print('Error posting')
+def post_comment(comment, reply_text):
+    try:
+        comment.reply(reply_text)
+        print('Comment Posted')
+    except:
+        print('Error posting')
 
 
 for comment in reddit.subreddit('all').stream.comments():
-	reply_text = process_comment(comment.body)
-	if reply_text is not None:
-		post_comment(comment, reply_text)
+    reply_text = process_comment(comment.body)
+    if reply_text is not None:
+        post_comment(comment, reply_text)
